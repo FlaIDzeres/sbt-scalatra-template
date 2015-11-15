ff46b2e9f5a0e8087877abf364f62c4694eb14a5
FlaIDzeres <FlaIDzeres@gmail.com>
2015-11-15 13:25:06 +0800

    servlet context config

---
+++ src/main/scala-2.11/com/flaidzeres/scala/sample/ScalatraBootstrap.scala
@@ -0,0 +1,15 @@
+package com.flaidzeres.scala.sample
+
+import javax.servlet.ServletContext
+
+import org.scalatra._
+
+/**
+ * Created by FlaID on 11/13/2015.
+ */
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context.mount(new SampleWebService, "/*")
+  }
+}
